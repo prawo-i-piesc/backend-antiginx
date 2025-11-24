7f716b252fb2d182b544ebdb897117c242b1717c
Obvinimus <obvinimus@protonmail.com>
2025-11-24 21:43:55 +0100

    feat: PostgresSQL database connection handling and .env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,23 +3,42 @@ package main
 import (
 	"log"
 
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 	"github.com/prawo-i-piesc/backend/internal/api"
 	"github.com/prawo-i-piesc/backend/internal/handlers"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	log.Println("Starting API server...")
-
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Info: Nie znaleziono pliku .env")
+	}
+	log.Println("Uruchamiam serwer API...")
+	conn_postgresql, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
-		log.Fatalf("Could not connect to RabbitMQ: %v", err)
+		log.Fatalf("Nie udało się połączyć z bazą danych: %v", err)
+	}
+	defer conn_postgresql.Close(context.Background())
+	var version string
+	if err := conn_postgresql.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
+		log.Fatalf("Nie udało się wykonać zapytania: %v", err)
+	}
+
+	log.Println("Połączono z:", version)
+
+	conn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
+	if err != nil {
+		log.Fatalf("Nie udało się połączyć z RabbitMQ: %v", err)
 	}
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("Could not open channel: %v", err)
+		log.Fatalf("Nie udało się otworzyć kanału: %v", err)
 	}
 	defer ch.Close()
 
