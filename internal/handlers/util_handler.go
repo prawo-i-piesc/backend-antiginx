7cb5dbe153580f17abdff4464db8c23714fd8d42
Obvinimus <95304608+Obvinimus@users.noreply.github.com>
2026-06-17 22:41:09 +0200

    119 podział testów na kategorie (#120)

--- internal/handlers/util_handler.go
+++ internal/handlers/util_handler.go
@@ -7,7 +7,5 @@ import (
 )
 
 func (h *ScanHandler) HandleAvailableScans(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"available_tests": AvailableTestsList,
-	})
+	c.JSON(http.StatusOK, CategorizedTests)
 }
